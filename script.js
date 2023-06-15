5923a457005dfd8ad84cea6b787968ef19dc32b1
sheks11 <110970118+sheks11@users.noreply.github.com>
2023-06-15 17:07:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let btn=document.getElementById("enter");
+let para=document.getElementById("status");
+
+btn.addEventListener("click",()=>{
+    para.style.display="none";
+    let head=document.createElement("h1");
+    head.innerHTML="Entered Metaverse"
+    document.body.appendChild(head);
+})
