8bc0918d099536c2e0e5a916015a44479e69640b
sheks11 <110970118+sheks11@users.noreply.github.com>
2023-06-15 17:25:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 //your JS code here. If required.
-let btn=document.getElementById("enter");
+let btn=document.getElementById("enterBtn");
 let para=document.getElementById("status");
+let head=document.getElementById("heading");
 
 btn.addEventListener("click",()=>{
+    // para.innerText="";
+    // let head=document.createElement("h1");
+    // head.innerHTML="Entered Metaverse"
+    // para.appendChild(head);
     para.innerText="";
-    let head=document.createElement("h1");
-    head.innerHTML="Entered Metaverse"
-    para.appendChild(head);
+    head.innerHTML="Entered Metaverse";
+
 })
 
